45ccbf8041839158a3fae2468fd8f75f737bf7ed
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2022-05-15 01:03:12 +0700

    Menambahkan kolom token

--- SIBOLA/database/migrations/2014_10_12_000000_create_users_table.php
+++ SIBOLA/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer("id_hak_akses");
+            $table->string("token") -> nullable();
         });
     }
 

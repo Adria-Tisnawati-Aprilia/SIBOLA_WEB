0b67b98c38b5a628c4e04c7bfa72de4047f3c88c
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2022-05-18 22:37:20 +0700

    Mengedit CRUD Lapangan

---
+++ SIBOLA/resources/views/owner/lapangan/edit.blade.php
@@ -0,0 +1,30 @@
+<input type="hidden" name="id" value="{{ $edit->id }}">
+<div class="form-group">
+    <label for="kode_lapangan"> Kode Lapangan </label>
+    <input type="text" name="kode_lapangan" id="kode_lapangan" class="form-control" placeholder="Masukkan Kode Lapangan" value="{{ $edit_lapangan->kode_lapangan }}">
+</div>
+<input type="hidden" name="id" value="{{ $edit->id }}">
+<div class="form-group">
+    <label for="nama_arena"> Nama Arena </label>
+    <input type="nama_arena" name="nama_arena" id="nama_arena" class="form-control" placeholder="Masukkan Nama Arena" value="{{ $edit_lapangan->nama_arena }}">
+</div>
+<input type="hidden" name="id" value="{{ $edit->id }}">
+<div class="form-group">
+    <label for="alas_lapangan"> Alas Lapangan</label>
+    <input type="text" name="alas_lapangan" id="alas_lapangan" class="form-control" placeholder="Masukkan Alas Lapangan" value="{{ $edit_lapangan->alas_lapangan }}">
+</div>
+<input type="hidden" name="id" value="{{ $edit->id }}">
+<div class="form-group">
+    <label for="foto"> Foto </label>
+    <input type="text" name="foto" id="foto" class="form-control" placeholder="Masukkan Foto" value="{{ $edit_lapangan->foto }}">
+</div>
+<input type="hidden" name="id" value="{{ $edit->id }}">
+<div class="form-group">
+    <label for="harga"> Harga </label>
+    <input type="harga" name="harga" id="harga" class="form-control" placeholder="Masukkan Harga" value="{{ $edit_lapangan->harga }}">
+</div>
+<input type="hidden" name="id" value="{{ $edit->id }}">
+<div class="form-group">
+    <label for="status"> Status</label>
+    <input type="text" name="status" id="status" class="form-control" placeholder="Masukkan Status" value="{{ $edit_lapangan->status }}">
+</div>
